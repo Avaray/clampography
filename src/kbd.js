1162732b2358071aed366a6589de5e0392730c46
Avaray <1459553+Avaray@users.noreply.github.com>
2026-05-29 08:03:29 +0200

    Update kbd.js

--- src/kbd.js
+++ src/kbd.js
@@ -44,7 +44,7 @@ export default (options = {}) => {
     [scope("kbd")]: {
       "display": "inline-block",
       "padding": "0.1em 0.45em",
-      "min-width": "2.1em",
+      "min-width": "2em",
       "text-align": "center",
       "font-size": "0.8em",
       "font-weight": "700",
